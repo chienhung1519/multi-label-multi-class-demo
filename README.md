adfa4aba4fdd52868fb989c01f3db941918f680e
chienhung1519 <chienhung1519@gmail.com>
2023-10-27 23:39:55 +0800

    dataset v1.1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # multi-label-multi-class-demo
 Multi-Label Multi-Class Bert Model with K-fold validation.
+
+## Aspects Distribution
+
+|                         |    1   |   2   |    3   |    4   |    5   | No filling in |
+|-------------------------|:------:|:-----:|:------:|:------:|:------:|:-------------:|
+| Check-in and boarding   |  5,174 | 3,213 |  9,852 | 27,992 | 71,757 |        69,188 |
+| Cleanliness             |  2,002 | 2,302 | 11,782 | 34,122 | 67,486 |        69,482 |
+| Customer service        | 10,217 | 4,860 | 10,277 | 29,053 | 89,878 |        42,891 |
+| Food and Beverage       |  5,441 | 6,383 | 26,995 | 33,184 | 42,761 |        72,412 |
+| In-flight Entertainment | 12,812 | 8,086 | 23,890 | 30,961 | 53,446 |        57,981 |
+| Legroom                 |  4,048 | 4,942 | 31,235 | 48,929 | 54,768 |        43,254 |
+| Seat comfort            |  4,747 | 5,803 | 30,449 | 53,195 | 49,971 |        43,011 |
+| Value for money         |  6,796 | 5,431 | 18,361 | 42,241 | 68,867 |        45,480 |
+
--- dataset_module.py
+++ dataset_module.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from datasets import Dataset
+import json
+from pathlib import Path
+
 
 class MultiLabelMultiClassDataset:
 
@@ -7,63 +10,89 @@ class MultiLabelMultiClassDataset:
         self.tokenizer = tokenizer
         self.args = args
 
+        # Load label mappings
+        self.aspect2id = json.loads(Path(self.args.resources_dir, "aspect_mapping.json").read_text())
+        self.id2aspect = {v: k for k, v in self.aspect2id.items()}
+        self.label2id = json.loads(Path(self.args.resources_dir, "label_mapping.json").read_text())
+        self.id2label = {v: k for k, v in self.label2id.items()}
+
+        # Load data
         self.train_data = self.load_data(self.args.train_data_path)
         self.val_data = self.load_data(self.args.val_data_path)
         self.test_data = self.load_data(self.args.test_data_path)
 
+        # Generate datasets
         self.train_dataset = self.generate_dataset(self.train_data)
         self.val_dataset = self.generate_dataset(self.val_data)
         self.test_dataset = self.generate_dataset(self.test_data)
-
-        self.label_mappings = {
-            label_name: {label: i for i, label in enumerate(self.train_data[label_name].unique())} 
-            for label_name in self.args.label_names
-        }
-        self.id_mappings = {
-            label_name: {i: label for i, label in self.label_mappings[label_name].items()} 
-            for label_name in self.args.label_names
-        }
 
     def load_data(self, path:str) -> pd.DataFrame:
         if path is None:
             return None
         data = pd.read_csv(path)
-        data = self.preprocess(data)
-        return data
+        data.columns = Path(self.args.resources_dir, "column_names.txt").read_text().splitlines()
+        return self.preprocess(data)
     
     def preprocess(self, data:pd.DataFrame) -> pd.DataFrame:
         if data is None:
             return None
         # Split aspect column into multiple columns
-        data[self.aspect_name] = data[self.aspect_name].apply(self.split_aspect)
-        for label_name in self.args.label_names:
-            data[label_name] = data[label_name].apply(lambda x: x[label_name])
-        return data
+        data["Aspects"] = data["Aspects"].apply(self.split_aspect)
+        for aspect in list(self.aspect2id.keys()):
+            data[aspect] = data["Aspects"].apply(
+                lambda x: self.score_to_label(x[aspect]) if aspect in x else self.label2id["NoFillingIn"]
+            )
+        return self.split_aspects_to_examples(data)
     
-    def split_aspect(self, aspect:str) -> dict:
-        aspects = aspect.split("|")
-        aspects = {aspect.split(":")[0]: float(aspect.split(":")[1]) for aspect in aspects}
-        return aspects
+    def split_aspect(self, raw_aspect:str) -> dict:
+        if raw_aspect == "No filling in":
+            return {}
+        return {aspect[:-2]: int(aspect[-1:]) for aspect in raw_aspect.split("|")}
+
+    def score_to_label(self, score:float) -> int:
+        if score <= 3:
+            return self.label2id["Negative"]
+        else:
+            return self.label2id["Positive"]
+    
+    def split_aspects_to_examples(self, data:pd.DataFrame) -> pd.DataFrame:
+        if data is None:
+            return None
+        examples = []
+        for _, row in data.iterrows():
+            for aspect in self.aspect2id.keys():
+                examples.append({
+                    "ReviewTitle": row["ReviewTitle"],
+                    "ReviewText": row["ReviewText"],
+                    "aspect_id": self.aspect_to_id(aspect),
+                    "labels": row[aspect]
+                })
+        return pd.DataFrame(examples)
 
     def generate_dataset(self, data:pd.DataFrame) -> Dataset:
         if data is None:
             return None
         dataset = Dataset.from_pandas(data)
-        return dataset.map(self.tokenize_function, batched=True)
+        return dataset.map(self.tokenize_function, batched=True, remove_columns=["ReviewTitle", "ReviewText"])
 
     def tokenize_function(self, examples:dict) -> dict:
         inputs = self.tokenizer(
-            examples[self.args.text_name], truncation=True, max_length=self.args.max_length, padding="max_length"
+            f"{examples["ReviewTitle"]}. {examples["ReviewText"]}", truncation=True, max_length=self.args.max_length, padding="max_length"
         )
-        for label_name in self.args.label_names:
-            inputs[label_name] = [self.label_to_id(label) for label in examples[label_name]]
         return inputs
 
-    def num_labels(self, label_name:str) -> int:
-        return len(self.label_mappings[label_name])
+    @property
+    def num_labels(self) -> int:
+        return len(self.label2id)
 
-    def label_to_id(self, label_name:str, label:str) -> int:
-        return self.label_mappings[label_name][label]
+    def label_to_id(self, label:str) -> int:
+        return self.label2id[label]
 
-    def id_to_label(self, label_name:str, id:int) -> str:
-        return self.id_mappings[label_name][id]
+    def id_to_label(self, id:int) -> str:
+        return self.id2label[id]
+
+    def aspect_to_id(self, aspect:str) -> int:
+        return self.aspect2id[aspect]
+
+    def id_to_aspect(self, id:int) -> str:
+        return self.id2aspect[id]
